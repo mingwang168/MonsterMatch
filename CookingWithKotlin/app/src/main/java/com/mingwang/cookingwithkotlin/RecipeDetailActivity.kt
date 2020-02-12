6b71e786b48a890613a4f995d18616be86f59518
mingwang168 <mingwang0904@gmail.com>
2020-02-12 14:47:42 -0800

    12

--- CookingWithKotlin/app/src/main/java/com/mingwang/cookingwithkotlin/RecipeDetailActivity.kt
+++ CookingWithKotlin/app/src/main/java/com/mingwang/cookingwithkotlin/RecipeDetailActivity.kt
@@ -3,26 +3,64 @@ package com.mingwang.cookingwithkotlin
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mingwang.cookingwithkotlin.R
+import com.mingwang.cookingwithkotlin.Recipe
 import kotlinx.android.synthetic.main.activity_recipe_detail.*
+import kotlinx.android.synthetic.main.recipe_steps.view.*
 
 class RecipeDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_detail)
-
-        val recipe: Recipe=intent?.extras?.getParcelable(recipeExtra)!!
-        recipe_name.text= recipe.name
-
+        val recipe: Recipe = intent?.extras?.getParcelable(recipeExtra)!!
+        recipe_name.text = recipe.name
+        Glide.with(this).load(recipe.imageURL).into(recipe_image)
+        recipe_steps.layoutManager = LinearLayoutManager(this)
+        recipe_steps.adapter = StepsAdapter(recipe.steps, this)
     }
 
-    companion object{
+    companion object {
         const val recipeExtra = "RECIPE"
 
-        fun newIntent(recipe: Recipe, context: Context) : Intent {
+        fun newIntent(recipe: Recipe, context: Context): Intent {
             val intent = Intent(context, RecipeDetailActivity::class.java)
             intent.putExtra(recipeExtra, recipe)
             return intent
         }
     }
 }
+
+private class StepsAdapter(
+    val steps: List<String>,
+    val context: Context
+) :
+    RecyclerView.Adapter<StepsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StepsViewHolder {
+        return StepsViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.recipe_steps,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return steps.count()
+    }
+
+    override fun onBindViewHolder(holder: StepsViewHolder, position: Int) {
+        val step = steps[position]
+        holder.itemView.recipe_step.text = step
+    }
+}
+
+private class StepsViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
